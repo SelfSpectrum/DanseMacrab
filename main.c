954b961897f7dffec5fea353021caac31ff86f14
Celes <celestemarmar2@gmail.com>
2024-02-11 05:25:29 -0300

    Trying to load data from csv, in order to archieve dialog

--- main.c
+++ main.c
@@ -1,3 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <raylib.h>
 #include <raymath.h>
 //---
@@ -10,8 +13,14 @@ struct Entity {
   Vector2 position;
   Vector2 size;
 };
-
+typedef struct Dialog Dialog;
+struct Dialog {
+  int id;
+  char *data[4];
+  int next;
+};
 void PlayerUpdate(Entity *player);
+void ParseDialog(char *line, Dialog *dialog);
 //------------------------------------------------------------------------------------
 // Program main entry point
 //------------------------------------------------------------------------------------
@@ -19,6 +28,15 @@ int main() {
   // Initialization
   //--------------------------------------------------------------------------------------
   Entity player = {10, {0, 0, 0, 255}, {0, 0}, {16, 16}};
+  Dialog dialog;
+  char *line;
+  FILE *file = fopen("./resources/dialog/data.csv", "r");
+  if (file == NULL) {
+    printf("Error opening the file!\n");
+    return 1;
+  }
+  int record = 0;
+  int target = 1;
   const int screenWidth = 800;
   const int screenHeight = 450;
   //Camera2D camera = {};
@@ -33,6 +51,12 @@ int main() {
   //--------------------------------------------------------------------------------------
 
   // Main game loop
+  
+  while (fgets(line, sizeof(line), file)) {
+    target++;
+    if (record == target) ParseDialog(line, &dialog);
+  }
+
   while (!exitWindow) {
     // Update
     //---------------------------------------------------------------------------------
@@ -54,10 +78,12 @@ int main() {
     BeginDrawing();
       ClearBackground(RAYWHITE);
       DrawRectangleV(player.position, player.size, player.color);
-    if (exitWindowRequested) {
-      DrawText("Are you sure you want to exit program? [Y/N]", 160, 200, 20, LIGHTGRAY);
-    }
-    else DrawText("Try to close the window to get confirmation message!", 120, 200, 20, LIGHTGRAY);
+      DrawText(dialog.data[0], 160, 400, 20, BLACK);
+      DrawText(dialog.data[1], 160, 440, 20, BLACK);
+      if (exitWindowRequested) {
+        DrawText("Are you sure you want to exit program? [Y/N]", 160, 200, 20, LIGHTGRAY);
+      }
+      else DrawText("Try to close the window to get confirmation message!", 120, 200, 20, LIGHTGRAY);
     EndDrawing();
     //----------------------------------------------------------------------------------
   }
@@ -74,4 +100,30 @@ void PlayerUpdate(Entity *player) {
   if (IsKeyDown(KEY_A)) player->position.x -= 1;
   if (IsKeyDown(KEY_S)) player->position.y += 1;
   if (IsKeyDown(KEY_D)) player->position.x += 1;
+}
+void ParseDialog(char *line, Dialog *dialog) {
+  char *token;
+  char *saveptr;
+  int i;
+  //Parse text
+  for (i = 0; i < 5; i++) {
+    token = strtok_r(NULL, ",", &saveptr);
+    if (token[i] == '"') {
+      // Handle quoted field
+      char *end_quote = strchr(token + 1, '"');
+      if (end_quote != NULL) {
+        // Copy the quoted string without the quotes
+        strncpy(dialog->data[i], token + 1, end_quote - token - 1);
+        dialog->data[i][end_quote - token - 1] = '\0';
+      } else {
+        // Error: unterminated quoted field
+        strcpy(dialog->data[i], "ERROR");
+      }
+    } else {
+      strcpy(dialog->data[i], token);
+    }
+  }
+  // Parse the value
+  token = strtok_r(NULL, ",", &saveptr);
+  dialog->next = atof(token);
 }

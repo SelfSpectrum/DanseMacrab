ced4212e04c356a0832f90ad96563167e5194a08
Celes <celestemarmar2@gmail.com>
2024-06-17 22:14:59 -0400

    Still working to fix the encoding failure

---
+++ README.md
@@ -0,0 +1,8 @@
+Assets license.
+Ramon Santamaria (@raysan5)
+
+** Fonts
+Pixelatus - Pixelatus 2
+UpheavalPro - Aleksandr Savenkov, Brian Kent
+PixelPerfect - Self Spectrum
+Pixel-UniCode - ivancr72
--- dechex.py
+++ dechex.py
@@ -1,17 +1,8 @@
 import sys
 
-def Main():
-    file = f'./resources/text/{sys.argv[1]}'
-    outp = f'{file}OUT.tsv'
-    file = f'{file}.tsv'
-    uftd = []
-    writ = []
-    with open(file) as tran:
-        for i in tran:
-            if not i[0].isnumeric():
-                writ.append(i[:-1:1])
-                continue
-            writ.append('\t'.join([i.split('\t')[0], (b''.join([chr(j).encode('utf-8') for j in [ord(k) if ord(k) != 32 else 0 for k in i.split('\t')[1]]])[:-3:1]).decode('utf-8')]))
-    with open(outp, 'w') as tran: tran.write('\n'.join(writ))
-if __name__ == "__main__":
-    Main();
+f=f'./resources/text/{sys.argv[1]}'
+o=f'{f}OUT.tsv'
+f=f'{f}.tsv'
+with open(f) as r:
+    with open(o, 'w') as w:
+        w.write('\n'.join([i if not i[0].isnumeric() else '\t'.join([i.split('\t')[0], (b''.join([chr(j).encode('utf-8') for j in [ord(k) for k in i.split('\t')[1]]]))]) for i in r]))
